a1e30acbd63c4c3712f3f60f9e1e2d5067bae44f
Carmine Varriale <C.Varriale@tudelft.nl>
2025-11-07 14:15:36 +0100

    Improve performance of isDirCosMat

--- @Orien/getDirCosMat.m
+++ @Orien/getDirCosMat.m
@@ -1,4 +1,4 @@
-function R = getDirCosMat(angleSeq, seqID)
+function dirCosMat = getDirCosMat(angleSeq, seqID)
 % Compound Direction Cosine Matrix used to express the coordinates of a
 % Vector of the Base frame in the Follower frame. Rotations are assumed
 % intrinsic, meaning that they happen over consecutive and intermediate axes.
@@ -7,11 +7,11 @@ arguments (Input)
 	seqID string
 end
 arguments (Output)
-	R {Orien.mustBeDirCosMat}
+	dirCosMat {Orien.mustBeDirCosMat}
 end
 switch seqID
 	case "321"
-		R = ...
+		dirCosMat = ...
 			getDirCosMat1(angleSeq(3)) * ...	% phi
 			getDirCosMat2(angleSeq(2)) * ...	% tht
 			getDirCosMat3(angleSeq(1)); ...	% psi
