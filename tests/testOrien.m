1365322927c5761b3218488e17ee53793685687a
Carmine Varriale <C.Varriale@tudelft.nl>
2025-11-12 10:56:32 +0100

    Update tests for new Frame interface

--- tests/testOrien.m
+++ tests/testOrien.m
@@ -17,6 +17,24 @@ classdef testOrien < matlab.unittest.TestCase
 
 	methods (Test)
 
+		function testCreateArrayOfOriens(testCase)
+			numOriens = 5;
+			oriensArray = Orien.empty(numOriens,0);
+			for i = 1:numOriens
+				oriensArray(i) = Orien(rand(3,1)*pi/2, "321", Frame());
+			end
+			testCase.verifyEqual(length(oriensArray), numOriens);
+
+			oriensArrayDefault(5) = Orien(rand(3,1)*pi/2, "321", Frame());
+			testCase.verifyEqual(oriensArrayDefault(1).coords, eye(3,3))
+			testCase.verifyEqual( ...
+				oriensArrayDefault(1).ref.uID, ...
+				"World")
+			testCase.verifyEqual( ...
+				oriensArrayDefault(2).ref.uID, ...
+				"World")
+		end
+
 		function testFromAnglesToDirCosMat(testCase)
 			testCase.verifyEqual( ...
 				Orien.getDirCosMat([pi/2; 0; 0], "321"), ...
--- tests/testPoint.m
+++ tests/testPoint.m
@@ -35,21 +35,35 @@ classdef testPoint < matlab.unittest.TestCase
 			testCase.world = World.getWorld();
 
 			testCase.fr1 = Frame("Frame1", ...
-				[1, 1, 1], [0, 0, 0], "321", testCase.world);
+				Point([1; 1; 1], testCase.world), ...
+				Orien([0; 0; 0], "321", testCase.world), ...
+				testCase.world);
 
 			testCase.fr2a = Frame("Frame2a", ...
-				[2, 0, 0], [0, 0, 0], "321", testCase.fr1);
+				Point([2; 0; 0], testCase.fr1), ...
+				Orien([0; 0; 0], "321", testCase.fr1), ...
+				testCase.fr1);
 			testCase.fr2b = Frame("Frame2b", ...
-				[0, 2, 0], [0, 0, 0], "321", testCase.fr1);
+				Point([0; 2; 0], testCase.fr1), ...
+				Orien([0; 0; 0], "321", testCase.fr1), ...
+				testCase.fr1);
 			testCase.fr2c = Frame("Frame2c", ...
-				[0, 0, 2], [0, 0, 0], "321", testCase.fr1);
+				Point([0; 0; 2], testCase.fr1), ...
+				Orien([0; 0; 0], "321", testCase.fr1), ...
+				testCase.fr1);
 
 			testCase.fr3a = Frame("Frame3a", ...
-				[0, 0, 0], [0, 0, pi/2], "321", testCase.fr2a);
+				Point([0; 0; 0], testCase.fr2a), ...
+				Orien([0; 0; pi/2], "321", testCase.fr2a), ...
+				testCase.fr2a);
 			testCase.fr3b = Frame("Frame3b", ...
-				[0, 0, 0], [0, pi/2, 0], "321", testCase.fr2b);
+				Point([0; 0; 0], testCase.fr2b), ...
+				Orien([0; pi/2; 0], "321", testCase.fr2b), ...
+				testCase.fr2b);
 			testCase.fr3c = Frame("Frame3c", ...
-				[0, 0, 0], [pi/2, 0, 0], "321", testCase.fr2c);
+				Point([0; 0; 0], testCase.fr2c), ...
+				Orien([pi/2; 0; 0], "321", testCase.fr2c), ...
+				testCase.fr2c);
 
 			testCase.addTeardown(@clear, "World", "getWorld")
 		end
@@ -61,7 +75,7 @@ classdef testPoint < matlab.unittest.TestCase
 
 		function testCreatePointInEmptyFrame(testCase)
 			try
-				Point(rand(1,3), Frame.empty());
+				Point(rand(3,1), Frame.empty());
 			catch ME
 				testCase.verifyMatches( ...
 					ME.identifier, ...
@@ -70,10 +84,29 @@ classdef testPoint < matlab.unittest.TestCase
 		end
 
 
+		function testCreateArrayOfPoints(testCase)
+			numPoints = 5;
+			pointsArray = Point.empty(numPoints,0);
+			for i = 1:numPoints
+				pointsArray(i) = Point(rand(3,1), testCase.world);
+			end
+			testCase.verifyEqual(length(pointsArray), numPoints);
+
+			pointsArrayDefault(5) = Point(rand(3,1), testCase.world);
+			testCase.verifyEqual(pointsArrayDefault(1).coords, [0; 0; 0])
+			testCase.verifyEqual( ...
+				pointsArrayDefault(1).ref.uID, ...
+				"World")
+			testCase.verifyEqual( ...
+				pointsArrayDefault(2).ref.uID, ...
+				"World")
+		end
+
+
 		function testGetPointCoords(testCase)
-			coords = rand(1,3);
+			coords = rand(3,1);
 			p = Point(coords, testCase.world);
-			expCoords = coords';
+			expCoords = coords;
 
 			testCase.verifyEqual(p.coords, expCoords);
 			testCase.verifyEqual(p.coords(1), expCoords(1));
@@ -83,7 +116,7 @@ classdef testPoint < matlab.unittest.TestCase
 
 
 		function testSetPointCoords(testCase)
-			p = Point(rand(1,3), testCase.world);
+			p = Point(rand(3,1), testCase.world);
 			expCoords1 = [1; 2; 3];
 			expCoords2 = [4; 5; 6];
 
@@ -92,18 +125,18 @@ classdef testPoint < matlab.unittest.TestCase
 			p.coords(3) = 3;
 			testCase.verifyEqual(p.coords, expCoords1)
 
-			p.coords = [4 5 6];
+			p.coords = [4; 5; 6];
 			testCase.verifyEqual(p.coords, expCoords2)
 		end
 
 
 		function testGetPointRadialVector(testCase)
-			coords = rand(1,3);
+			coords = rand(3,1);
 			p = Point(coords, testCase.world);
-			expCoords = coords';
+			expCoords = coords;
 
 			testCase.verifyEqual(p.radius.coords, expCoords);
-			testCase.verifyEqual(p.radius.mag, norm(expCoords));
+			testCase.verifyEqual(p.radius.magnitude, norm(expCoords));
 			testCase.verifyEqual(p.radius.ref, testCase.world);
 		end
 
@@ -112,19 +145,19 @@ classdef testPoint < matlab.unittest.TestCase
 			coords = [10;10;10];
 
 			p1 = Point(coords, testCase.fr1);
-			testCase.verifyEqual(p1.viewIn(testCase.fr1), [10;10;10]);
-			testCase.verifyEqual(p1.viewIn(testCase.world), [11;11;11]);
-			testCase.verifyEqual(p1.viewIn(testCase.fr2a), [8;10;10]);
-			testCase.verifyEqual(p1.viewIn(testCase.fr2b), [10;8;10]);
-			testCase.verifyEqual(p1.viewIn(testCase.fr2c), [10;10;8]);
-			testCase.verifyEqual(p1.viewIn(testCase.fr3a), [8;10;-10]);
-			testCase.verifyEqual(p1.viewIn(testCase.fr3b), [-10;8;10]);
-			testCase.verifyEqual(p1.viewIn(testCase.fr3c), [10;-10;8]);
+			testCase.verifyEqual(p1.resolveIn(testCase.fr1).coords, [10;10;10]);
+			testCase.verifyEqual(p1.resolveIn(testCase.world).coords, [11;11;11]);
+			testCase.verifyEqual(p1.resolveIn(testCase.fr2a).coords, [8;10;10]);
+			testCase.verifyEqual(p1.resolveIn(testCase.fr2b).coords, [10;8;10]);
+			testCase.verifyEqual(p1.resolveIn(testCase.fr2c).coords, [10;10;8]);
+			testCase.verifyEqual(p1.resolveIn(testCase.fr3a).coords, [8;10;-10]);
+			testCase.verifyEqual(p1.resolveIn(testCase.fr3b).coords, [-10;8;10]);
+			testCase.verifyEqual(p1.resolveIn(testCase.fr3c).coords, [10;-10;8]);
 
 			p3c = Point(coords, testCase.fr3c);
-			testCase.verifyEqual(p3c.viewIn(testCase.fr2c), [-10;10;10]);
-			testCase.verifyEqual(p3c.viewIn(testCase.world), [-9;11;13]);
-			testCase.verifyEqual(p3c.viewIn(testCase.fr3a), [-12;12;-10]);
+			testCase.verifyEqual(p3c.resolveIn(testCase.fr2c).coords, [-10;10;10]);
+			testCase.verifyEqual(p3c.resolveIn(testCase.world).coords, [-9;11;13]);
+			testCase.verifyEqual(p3c.resolveIn(testCase.fr3a).coords, [-12;12;-10]);
 		end
 
 
@@ -134,16 +167,11 @@ classdef testPoint < matlab.unittest.TestCase
 			v2 = Vector([0 1 0], testCase.world);
 			v3 = Vector([0 0 1], testCase.world);
 			v4 = Vector([-2 -1 -1], testCase.world);
-
-			expCoords1 = [2 0 0]';
-			expCoords2 = [2 1 0]';
-			expCoords3 = [2 1 1]';
-			expCoords4 = [0 0 0]';
-
-			testCase.verifyEqual(p1.translate(v1).coords, expCoords1);
-			testCase.verifyEqual(p1.translate(v2).coords, expCoords2);
-			testCase.verifyEqual(p1.translate(v3).coords, expCoords3);
-			testCase.verifyEqual(p1.translate(v4).coords, expCoords4);
+			testCase.verifyEqual(p1.translate(v1).coords, [2; 0; 0]);
+			testCase.verifyEqual(p1.translate(v2).coords, [1; 1; 0]);
+			testCase.verifyEqual( ...
+				p1.translate(v3).translate(v4).coords, ...
+				[-1; -1; 0]);
 			testCase.verifyError( ...
 				@() p1.translate(Vector([0 0 0], testCase.fr1)), ...
 				"Point:translate:sameRef");
@@ -157,22 +185,31 @@ classdef testPoint < matlab.unittest.TestCase
 			r1 = Orien([pi/2, 0, 0], "321", testCase.world);
 			r2 = Orien([0, -3*pi/2, 0], "321", testCase.world);
 			r3 = Orien([0, 0, -pi/2], "321", testCase.world);
-			r4 = Orien([-pi/4, 0, 0], "321", testCase.world);
+			r4 = Orien([pi/4, 0, 0], "321", testCase.world);
 
-			testCase.verifyEqual(p1.rotate(r1,p0).coords, [0; 1; 0]);
-			testCase.verifyEqual(p1.rotate(r2,p0).coords, [0; 1; 0]);
-			testCase.verifyEqual(p1.rotate(r1,p0).coords, [-1; 0; 0]);
-			testCase.verifyEqual(p1.rotate(r2,p0).coords, [0; 0; 1]);
-			testCase.verifyEqual(p1.rotate(r3,p0).coords, [0; 1; 0]);
 			testCase.verifyEqual( ...
-				p1.rotate(r4,p0).coords, [sqrt(2); sqrt(2); 0]/2, ...
+				p1.rotate(r1,p0).coords, ...
+				[0; 1; 0]);
+			testCase.verifyEqual( ...
+				p1.rotate(r2,p0).coords, ...
+				[0; 0; -1]);
+			testCase.verifyEqual( ...
+				p1.rotate(r1,p0).rotate(r2,p0).rotate(r1,p0).coords, ...
+				[-1; 0; 0]);
+			testCase.verifyEqual( ...
+				p1.rotate(r3,p0).coords, ...
+				[1; 0; 0]);
+			testCase.verifyEqual( ...
+				p1.rotate(r4,p0).coords, ...
+				[sqrt(2); sqrt(2); 0]/2, ...
 				AbsTol=1e-12);
 			testCase.verifyEqual( ...
-				p1.rotate(r4,p0).coords, [1; 0; 0], ...
+				p1.rotate(r4,p0).rotate(r4,p0).coords, ...
+				[0; 1; 0], ...
 				AbsTol=1e-12);
 			testCase.verifyError( ...
 				@() p1.rotate(r1, Point([0; 0; 0], testCase.fr1)), ...
-				"Point:calcRotate:sameRef");
+				"Point:rotate:sameRef");
 		end
 
 	end
